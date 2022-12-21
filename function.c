4ab3f47742cbe5fdeb1882104ad3b8caecb4d6e8
KatGORA <113862278+KatGORA@users.noreply.github.com>
2022-12-21 02:16:39 -0500

    Add files via upload

---
+++ function.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// Create a function
+void printSomething() {
+    printf("Something");
+
+}
+
+int main() {
+  printSomething(); // call the function
+  return 0;
+}
+
+// Outputs "Something"
