7fdb71923db6e96e45035db9f551cfe1afe61543
KatGORA <113862278+KatGORA@users.noreply.github.com>
2022-12-21 01:59:01 -0500

    Add files via upload
    example of a struct in C

---
+++ structs.c
@@ -0,0 +1,11 @@
+struct myStructure {
+    //could be called BlackTail if you wanted
+  int myNum;
+  char myLetter;
+};
+
+int main() {
+  struct myStructure s1; 
+  // s1 is used to access information in struct myStructure
+  return 0;
+}
